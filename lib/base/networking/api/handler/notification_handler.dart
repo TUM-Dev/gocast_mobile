0890d634e68581f93796ce8aba8f59a7cce9a835
Milena Rode-Kotzé <milena.rodekotze@gmail.com>
2023-12-21 20:52:10 +0100

    Create notification_handler
    for BannerAlerts and FeatureNotifications

---
+++ lib/base/networking/api/handler/notification_handler.dart
@@ -0,0 +1,39 @@
+import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
+import 'package:gocast_mobile/base/networking/api/handler/grpc_handler.dart';
+import 'package:logger/logger.dart';
+
+/// Handles course-related data operations.
+///
+/// This class is responsible for fetching and posting course-related data, such as fetching public courses and semesters.
+class NotificationHandler {
+  static final Logger _logger = Logger();
+  final GrpcHandler _grpcHandler;
+
+  NotificationHandler(this._grpcHandler);
+
+  Future<List<FeatureNotification>> fetchFeatureNotifications() async {
+    _logger.i('Fetching public courses');
+    return _grpcHandler.callGrpcMethod(
+      (client) async {
+        final response = await client
+            .getFeatureNotifications(GetFeatureNotificationsRequest());
+        _logger.d('Public courses: ${response.featureNotifications}');
+        return response.featureNotifications;
+      },
+    );
+  }
+
+  Future<List<BannerAlert>> fetchBannerAlerts() async {
+    _logger.i('Fetching public courses');
+    return _grpcHandler.callGrpcMethod(
+          (client) async {
+        final response = await client
+            .getBannerAlerts(GetBannerAlertsRequest());
+        _logger.d('Public courses: ${response.bannerAlerts}');
+        return response.bannerAlerts;
+      },
+    );
+  }
+
+  //TODO push notifications
+}
