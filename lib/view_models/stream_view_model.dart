f940b8ee32afd1e4bcaae949cc11c4b33d571d8d
Achraf Labidi <achraf.labidi@tum.de>
2023-12-23 20:26:54 +0100

    Fetch stream in PinnedView

--- lib/view_models/stream_view_model.dart
+++ lib/view_models/stream_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:fixnum/fixnum.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
 import 'package:gocast_mobile/base/networking/api/handler/grpc_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/stream_handler.dart';
 import 'package:gocast_mobile/models/error/error_model.dart';
@@ -12,16 +13,18 @@ class StreamViewModel extends StateNotifier<StreamState> {
 
   StreamViewModel(this._grpcHandler) : super(const StreamState());
 
-  Future<void> fetchCourseStreams(int courseID) async {
+  Future<List<Stream>> fetchCourseStreams(int courseID) async {
     _logger.i('Fetching streams');
     state = state.copyWith(isLoading: true);
     try {
       final streams =
           await StreamHandler(_grpcHandler).fetchCourseStreams(courseID);
       state = state.copyWith(streams: streams, isLoading: false);
+      return streams;
     } catch (e) {
       _logger.e(e);
       state = state.copyWith(error: e as AppError, isLoading: false);
+      return [];
     }
   }
 
--- lib/views/course_view/pinned_courses_view/pinned_courses_view.dart
+++ lib/views/course_view/pinned_courses_view/pinned_courses_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gocast_mobile/providers.dart';
 import 'package:gocast_mobile/views/course_view/components/pinned_course_card.dart';
 import 'package:gocast_mobile/views/course_view/pinned_courses_view/pinned_courses_content_view.dart';
+import 'package:gocast_mobile/views/video_view/video_player.dart';
+import 'package:gocast_mobile/views/video_view/video_player_controller.dart';
 
 class PinnedCourses extends ConsumerStatefulWidget {
   const PinnedCourses({super.key});
@@ -28,6 +30,7 @@ class PinnedCoursesState extends ConsumerState<PinnedCourses> {
   @override
   Widget build(BuildContext context) {
     final userPinned = ref.watch(userViewModelProvider).userPinned ?? [];
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
 
     return RefreshIndicator(
       onRefresh: _refreshPinnedCourses,
@@ -39,19 +42,64 @@ class PinnedCoursesState extends ConsumerState<PinnedCourses> {
           return PinnedCourseCard(
             imageName: 'assets/images/course2.png',
             course: course,
-            onTap: () {},
+            onTap: () async {
+              try {
+                await ref
+                    .read(videoViewModelProvider.notifier)
+                    .fetchCourseStreams(course.id);
+                if (!mounted) {
+                  return; // Check if the widget is still in the tree
+                }
+                var stream =
+                    ref.read(videoViewModelProvider).streams?.firstOrNull;
+                if (stream != null) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => VideoPlayerPage(
+                        videoSource: stream.playlistUrl,
+                        title: course.name,
+                        sourceType: determineSourceType(stream.playlistUrl),
+                      ),
+                    ),
+                  );
+                } else {
+                  if (!mounted) return;
+                  scaffoldMessenger.showSnackBar(
+                    const SnackBar(
+                      content:
+                          Text("No video stream available for this course."),
+                    ),
+                  );
+                }
+              } catch (e) {
+                if (!mounted) return;
+                scaffoldMessenger.showSnackBar(
+                  SnackBar(
+                    content: Text("Failed to load course streams: $e"),
+                  ),
+                );
+              }
+            },
             isPinned: isPinned,
-            onPinToggle: () {
+            onPinToggle: () async {
               final viewModel = ref.read(userViewModelProvider.notifier);
               if (isPinned) {
-                viewModel.unpinCourse(course.id);
+                await viewModel.unpinCourse(course.id);
               } else {
-                viewModel.pinCourse(course.id);
+                await viewModel.pinCourse(course.id);
               }
             },
           );
         }).toList(),
       ),
     );
+  }
+
+  VideoSourceType determineSourceType(String videoSource) {
+    // Logic to determine if the source is an asset or a network source
+    return videoSource.startsWith('http')
+        ? VideoSourceType.network
+        : VideoSourceType.asset;
   }
 }
