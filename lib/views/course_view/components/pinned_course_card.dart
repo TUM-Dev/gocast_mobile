47ca587ef2fd382506d74ca8f97c25e4e6923605
ge87joq <betul.cimendag@tum.de>
2023-12-27 21:57:52 +0100

    Fix padding Pinned Course Card on pinned course view

--- lib/views/course_view/components/pinned_course_card.dart
+++ lib/views/course_view/components/pinned_course_card.dart
@@ -32,61 +32,64 @@ class PinnedCourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${course.name} - ${course.slug}',
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${course.name} - ${course.slug}',
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        "${course.semester.year} ${course.semester.teachingTerm}",
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
+                        const SizedBox(height: 8.0),
+                        Text(
+                          "${course.semester.year} ${course.semester.teachingTerm}",
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    isPinned ? Icons.push_pin : Icons.push_pin_outlined,
-                    color: Colors.blue,
+                  IconButton(
+                    icon: Icon(
+                      isPinned ? Icons.push_pin : Icons.push_pin_outlined,
+                      color: Colors.blue[800],
+                    ),
+                    onPressed: onPinToggle,
                   ),
-                  onPressed: onPinToggle,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          AspectRatio(
-            aspectRatio: 16 / 9,
-            child: Image.asset(
-              imageName,
-              fit: BoxFit.cover,
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.asset(
+                imageName,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 12.0),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 12.0),
+            ),
+          ],
+        ),
       ),
     );
   }
