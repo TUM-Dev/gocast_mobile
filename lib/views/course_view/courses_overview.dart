9451f4239305556bb36c4720b368a8c11099912c
Milena Rode-Kotzé <milena.rodekotze@gmail.com>
2023-12-28 14:47:01 +0100

    Re-correct course screen title
    Back to the original 'gocast'

--- lib/views/course_view/courses_overview.dart
+++ lib/views/course_view/courses_overview.dart
@@ -40,7 +40,7 @@ class CourseOverviewState extends ConsumerState<CourseOverview> {
 
     return BaseView(
       showLeading: false,
-      title: 'Courses',
+      title: 'GoCast',
       actions: [
         IconButton(
           icon: const Icon(Icons.settings),
