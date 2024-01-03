120ddd372bbe3575415045f14ffdd979b2881694
ge59dil <anish.goyal@tum.de>
2024-01-03 16:05:18 +0100

    some more changes for dark mode, added _loadtheme preference

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-// Define a class for custom colors used throughout the app
+// Define a class for custom colors used throughout the app (Light Theme)
 class AppColors {
-  static const Color primaryColor =
-      Color(0xFF0D47A1); // ARGB value of Colors.blue[900]
+  static const Color primaryColor = Color(0xFF0D47A1); // Colors.blue[900]
   static const Color primaryTextColor = Colors.black;
   static const Color secondaryTextColor = Colors.black54;
   static const Color appBarBackgroundColor = Colors.white;
@@ -16,6 +15,20 @@ class AppColors {
   static const Color tumBlue = Color(0xFF0065BD);
 }
 
+// Define a class for custom colors used throughout the app (Dark Theme)
+class DarkAppColors {
+  static const Color primaryColor = Color(0xFF0D47A1); // Same as light theme
+  static const Color primaryTextColor = Colors.white;
+  static const Color secondaryTextColor = Colors.white70;
+  static const Color appBarBackgroundColor = Colors.black;
+  static const Color appBarIconColor = Colors.white;
+  static const Color appBarTextColor = Colors.white;
+  static const Color sectionTitleColor = Colors.white;
+  static const Color indicatorActiveColor = Colors.blue;
+  static const Color linkTextColor = Colors.lightBlueAccent;
+  static const Color inactiveColor = Color(0xFF757575);
+}
+
 // Define a class for custom text styles
 class AppTextStyles {
   static const TextStyle sectionTitle = TextStyle(
@@ -23,18 +36,15 @@ class AppTextStyles {
     fontWeight: FontWeight.bold,
     color: AppColors.sectionTitleColor,
   );
-
   static const TextStyle heading = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.bold,
     color: AppColors.primaryTextColor,
   );
-
   static const TextStyle body = TextStyle(
     fontSize: 16,
     color: AppColors.secondaryTextColor,
   );
-
   static const TextStyle link = TextStyle(
     decoration: TextDecoration.underline,
     fontSize: 16,
@@ -42,7 +52,30 @@ class AppTextStyles {
   );
 }
 
-// Define the app's theme data
+// Define a class for custom text styles (Dark Theme)
+class DarkAppTextStyles {
+  static const TextStyle sectionTitle = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: DarkAppColors.sectionTitleColor,
+  );
+  static const TextStyle heading = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: DarkAppColors.primaryTextColor,
+  );
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    color: DarkAppColors.secondaryTextColor,
+  );
+  static const TextStyle link = TextStyle(
+    decoration: TextDecoration.underline,
+    fontSize: 16,
+    color: DarkAppColors.linkTextColor,
+  );
+}
+
+// Define the app's light theme data
 final ThemeData appTheme = ThemeData(
   primaryColor: AppColors.primaryColor,
   scaffoldBackgroundColor: AppColors.appBarBackgroundColor,
@@ -82,6 +115,50 @@ final ThemeData appTheme = ThemeData(
     ),
     focusedBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: AppColors.primaryColor, width: 2.0),
+    ),
+  ),
+);
+
+// Define the app's dark theme data
+final ThemeData darkAppTheme = ThemeData(
+  primaryColor: DarkAppColors.primaryColor,
+  scaffoldBackgroundColor: DarkAppColors.appBarBackgroundColor,
+  colorScheme: ColorScheme.dark().copyWith(
+    primary: DarkAppColors.primaryColor,
+    secondary: DarkAppColors.indicatorActiveColor,
+  ),
+  appBarTheme: AppBarTheme(
+    color: DarkAppColors.appBarBackgroundColor,
+    iconTheme: IconThemeData(color: DarkAppColors.appBarIconColor),
+    titleTextStyle: TextStyle(
+      color: DarkAppColors.appBarTextColor,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  textTheme: TextTheme(
+    titleMedium: DarkAppTextStyles.sectionTitle,
+    displayLarge: DarkAppTextStyles.heading,
+    bodyLarge: DarkAppTextStyles.body,
+    titleSmall: DarkAppTextStyles.link,
+  ),
+  buttonTheme: ButtonThemeData(
+    buttonColor: DarkAppColors.primaryColor,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+    textTheme: ButtonTextTheme.primary,
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+    hintStyle: TextStyle(
+      color: Colors.grey[400],
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xFF757575), width: 1.0),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: DarkAppColors.primaryColor, width: 2.0),
     ),
   ),
 );
