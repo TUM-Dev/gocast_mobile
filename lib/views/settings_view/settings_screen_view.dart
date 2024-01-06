6f20cbc1e42e5642a9b097a3c89eee731ab3a6ba
ge87joq <betul.cimendag@tum.de>
2024-01-06 01:22:11 +0100

    Fix bug when user selects multiple playback speeds on Settings View

--- lib/views/settings_view/settings_screen_view.dart
+++ lib/views/settings_view/settings_screen_view.dart
@@ -7,6 +7,9 @@ import 'package:gocast_mobile/views/on_boarding_view/welcome_screen_view.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:gocast_mobile/views/settings_view/edit_profile_screen_view.dart';
 import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
+import 'package:gocast_mobile/views/settings_view/custom_playback_speed_view.dart';
+import 'package:gocast_mobile/views/settings_view/playback_speed_picker_view.dart';
+import 'package:gocast_mobile/views/settings_view/authentication_error_card_view.dart';
 
 class SettingsScreen extends ConsumerStatefulWidget {
   const SettingsScreen({super.key});
@@ -16,6 +19,8 @@ class SettingsScreen extends ConsumerStatefulWidget {
 }
 
 class _SettingsScreenState extends ConsumerState<SettingsScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   bool isDarkMode = false;
   bool isPushNotificationsEnabled = false;
   bool isDownloadOverWifiOnly = false;
@@ -28,6 +33,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     ref.read(userViewModelProvider.notifier).fetchUserSettings();
     _loadThemePreference();
     _loadNotificationPreference();
+    _loadUserSettings();
   }
 
   Future<void> _loadThemePreference() async {
@@ -56,104 +62,102 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     await prefs.setBool('notifications', value);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // Update isDarkMode based on the provider
-    final themeMode = ref.watch(themeModeProvider);
-    isDarkMode = themeMode == ThemeMode.dark;
-    final userState = ref.watch(userViewModelProvider);
-
-    final currentGreeting = userState.userSettings
-            ?.firstWhere(
-              (setting) => setting.type == UserSettingType.GREETING,
-              orElse: () => UserSetting(value: 'Default Greeting'),
-            )
-            .value ??
-        'Default Greeting';
-
-    if (preferredGreeting != currentGreeting) {
-      setState(() {
-        preferredGreeting = currentGreeting;
-      });
-    }
-
-    selectedPlaybackSpeeds = [1.0];
+  void _loadUserSettings() {
+    final userState = ref.read(userViewModelProvider);
+    final greetingSetting = userState.userSettings?.firstWhere(
+      (setting) => setting.type == UserSettingType.GREETING,
+      orElse: () => UserSetting(value: 'Default Greeting'),
+    );
+    preferredGreeting = greetingSetting?.value ?? 'Default Greeting';
 
     final playbackSpeedSetting = userState.userSettings?.firstWhere(
       (setting) => setting.type == UserSettingType.CUSTOM_PLAYBACK_SPEEDS,
-      orElse: () => UserSetting(value: 'values'),
+      orElse: () => UserSetting(value: jsonEncode([])),
     );
 
     if (playbackSpeedSetting != null && playbackSpeedSetting.value.isNotEmpty) {
       try {
-        // Decode the JSON string
         List<dynamic> speedsList = jsonDecode(playbackSpeedSetting.value);
-        if (kDebugMode) {
-          print('Decoded speeds list: $speedsList');
-        }
         selectedPlaybackSpeeds = speedsList
             .where((item) => item is Map && item['enabled'] == true)
             .map((item) => item['speed'] as double)
             .toList();
-        if (kDebugMode) {
-          print('Selected playbacks: $selectedPlaybackSpeeds');
-        }
       } catch (e) {
         if (kDebugMode) {
           print('Error parsing playback speeds: $e');
         }
       }
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeMode = ref.watch(themeModeProvider);
+    var isDarkMode = themeMode == ThemeMode.dark;
 
     return Scaffold(
+      key: _scaffoldKey,
       appBar: _buildAppBar(context),
-      body: ListView(
-        children: [
-          _buildProfileTile(),
-          const Divider(),
-          _buildSectionTitle('Account Settings'),
-          _buildEditableListTile('Edit profile', () {}),
-          _buildPreferredGreetingTile(),
-          _buildSwitchListTile(
-            title: 'Push notifications',
-            value: isPushNotificationsEnabled,
-            onChanged: (value) {
-              setState(() => isPushNotificationsEnabled = value);
-              _saveNotificationPreference(value);
-            },
-          ),
-          _buildSwitchListTile(
-            title: 'Dark mode',
-            value: isDarkMode,
-            onChanged: (value) {
-              setState(() => isDarkMode = value);
-              ref.read(themeModeProvider.notifier).state =
-                  value ? ThemeMode.dark : ThemeMode.light;
-              _saveThemePreference(value ? 'dark' : 'light');
-            },
-          ),
-          _buildSwitchListTile(
-            title: 'Download Over Wi-Fi only',
-            value: isDownloadOverWifiOnly,
-            onChanged: (value) =>
-                setState(() => isDownloadOverWifiOnly = value),
-          ),
-          _buildSectionTitle('Video Playback Speed'),
-          _buildPlaybackSpeedsTile(),
-          _buildCustomPlaybackSpeedsTile(),
-          _buildLogoutTile(context),
-          const Divider(),
-          _buildSectionTitle('More'),
-          _buildNavigableListTile('About us', () {
-            // TODO: Navigate to about us screen
-          }),
-          _buildNavigableListTile('Privacy policy', () {
-            // TODO: Navigate to privacy policy screen
-          }),
-          _buildNavigableListTile('Terms and conditions', () {
-            // TODO: Navigate to terms and conditions screen
-          }),
-        ],
+      body: Consumer(
+        builder: (context, watch, _) {
+          final userState = ref.watch(userViewModelProvider);
+          return ListView(
+            children: [
+              _buildProfileTile(userState),
+              const Divider(),
+              _buildSectionTitle('Account Settings'),
+              _buildEditableListTile('Edit profile', () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => EditProfileScreen(
+                      updatePreferredName: _updatePreferredNameAPI,
+                    ),
+                  ),
+                );
+              }),
+              _buildPreferredGreetingTile(),
+              _buildSwitchListTile(
+                title: 'Push notifications',
+                value: isPushNotificationsEnabled,
+                onChanged: (value) {
+                  setState(() => isPushNotificationsEnabled = value);
+                  _saveNotificationPreference(value);
+                },
+              ),
+              _buildSwitchListTile(
+                title: 'Dark mode',
+                value: isDarkMode,
+                onChanged: (value) {
+                  setState(() => isDarkMode = value);
+                  ref.read(themeModeProvider.notifier).state =
+                      value ? ThemeMode.dark : ThemeMode.light;
+                  _saveThemePreference(value ? 'dark' : 'light');
+                },
+              ),
+              _buildSwitchListTile(
+                title: 'Download Over Wi-Fi only',
+                value: isDownloadOverWifiOnly,
+                onChanged: (value) =>
+                    setState(() => isDownloadOverWifiOnly = value),
+              ),
+              _buildSectionTitle('Video Playback Speed'),
+              _buildPlaybackSpeedsTile(),
+              _buildCustomPlaybackSpeedsTile(context),
+              _buildLogoutTile(context),
+              const Divider(),
+              _buildSectionTitle('More'),
+              _buildNavigableListTile('About us', () {
+                // TODO: Navigate to about us screen
+              }),
+              _buildNavigableListTile('Privacy policy', () {
+                // TODO: Navigate to privacy policy screen
+              }),
+              _buildNavigableListTile('Terms and conditions', () {
+                // TODO: Navigate to terms and conditions screen
+              }),
+            ],
+          );
+        },
       ),
     );
   }
@@ -205,100 +209,21 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     return ListTile(
       title: const Text('Playback Speeds'),
       trailing: const Icon(Icons.arrow_forward_ios),
-      onTap: () => _showPlaybackSpeedsPicker(),
+      onTap: () => showPlaybackSpeedsPicker(
+        context,
+        selectedPlaybackSpeeds,
+        _updateSelectedSpeeds,
+      ),
     );
   }
 
-  ListTile _buildCustomPlaybackSpeedsTile() {
+  ListTile _buildCustomPlaybackSpeedsTile(BuildContext context) {
     return ListTile(
       title: const Text('Add Custom Playback Speed'),
-      onTap: () => _showAddCustomSpeedDialog(),
-    );
-  }
-
-  void _showPlaybackSpeedsPicker() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: 14,
-                itemBuilder: (context, index) {
-                  final double speed = (index + 1) * 0.25;
-                  return ListTile(
-                    title: Padding(
-                      padding: const EdgeInsets.only(left: 16.0, top: 10.0),
-                      child: Text('${speed}x'),
-                    ),
-                    trailing: selectedPlaybackSpeeds.contains(speed)
-                        ? const Icon(Icons.check)
-                        : const SizedBox(),
-                    onTap: () {
-                      bool isSelected = selectedPlaybackSpeeds.contains(speed);
-                      _updateSelectedSpeeds(speed, !isSelected);
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _showAddCustomSpeedDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        double customSpeed = 1.0;
-        return Dialog(
-          child: Material(
-            type: MaterialType.card,
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const Text('Add Custom Speed',
-                      style: TextStyle(fontSize: 18)),
-                  const SizedBox(height: 16),
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Enter speed (e.g., 1.7)',
-                    ),
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    onChanged: (value) {
-                      customSpeed = double.tryParse(value) ?? 1.0;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('Cancel'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: const Text('Add'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          _updateSelectedSpeeds(customSpeed, true);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
+      onTap: () {
+        showAddCustomSpeedDialog(context, (double customSpeed) {
+          _updateSelectedSpeeds(customSpeed, true);
+        });
       },
     );
   }
@@ -314,54 +239,76 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     _updatePlaybackSpeedsAPI(selectedPlaybackSpeeds);
   }
 
-  void _updatePreferredGreetingAPI(String greeting) {
+  void _updatePreferredNameAPI(String name) {
     ref.read(userViewModelProvider.notifier).updateUserSettings([
-      UserSetting(type: UserSettingType.GREETING, value: greeting),
-    ]);
+      UserSetting(type: UserSettingType.PREFERRED_NAME, value: name),
+    ]).then((_) {
+      // Fetch all settings again to ensure the state is up-to-date
+      ref.read(userViewModelProvider.notifier).fetchUserSettings();
+    });
   }
 
-  void _updatePlaybackSpeedsAPI(List<double> speeds) {
-    List<Map<String, dynamic>> speedsList = speeds
-        .map(
-          (speed) => {
-            "speed": speed,
-            "enabled": true,
-          },
-        )
-        .toList();
+  void _updatePreferredGreetingAPI(String greeting) async {
+    bool isAuthenticated = await showAuthenticationErrorCard(_scaffoldKey, ref);
+    if (isAuthenticated) {
+      ref.read(userViewModelProvider.notifier).updateUserSettings([
+        UserSetting(type: UserSettingType.GREETING, value: greeting),
+      ]).then((_) {
+        ref.read(userViewModelProvider.notifier).fetchUserSettings();
+      });
+    }
+  }
 
-    final String speedsJson = jsonEncode(speedsList);
+  void _updatePlaybackSpeedsAPI(List<double> speeds) async {
+    bool isAuthenticated = await showAuthenticationErrorCard(_scaffoldKey, ref);
+    if (isAuthenticated) {
+      List<Map<String, dynamic>> speedsList = speeds
+          .map(
+            (speed) => {
+              "speed": speed,
+              "enabled": true,
+            },
+          )
+          .toList();
 
-    final UserSetting playbackSpeedSetting = UserSetting(
-      type: UserSettingType.CUSTOM_PLAYBACK_SPEEDS,
-      value: speedsJson,
+      final String speedsJson = jsonEncode(speedsList);
+
+      final UserSetting playbackSpeedSetting = UserSetting(
+        type: UserSettingType.CUSTOM_PLAYBACK_SPEEDS,
+        value: speedsJson,
+      );
+
+      ref
+          .read(userViewModelProvider.notifier)
+          .updateUserSettings([playbackSpeedSetting]).then((_) {
+        ref.read(userViewModelProvider.notifier).fetchUserSettings();
+      });
+    }
+  }
+
+  ListTile _buildProfileTile(userState) {
+    // Extract settings from userState
+    final preferredNameSetting = userState.userSettings?.firstWhere(
+      (setting) => setting.type == UserSettingType.PREFERRED_NAME,
+      orElse: () => UserSetting(value: ''),
     );
 
-    ref
-        .read(userViewModelProvider.notifier)
-        .updateUserSettings([playbackSpeedSetting]);
-  }
+    final preferredName = preferredNameSetting?.value ?? '';
+    final userName = userState.user?.name ?? 'Guest';
 
-  ListTile _buildProfileTile() {
-    final greeting = ref
-        .watch(userViewModelProvider)
-        .userSettings
-        ?.firstWhere(
-          (setting) => setting.type == UserSettingType.GREETING,
-          orElse: () => UserSetting(value: 'Hi'),
-        )
-        .value;
+    final greetingSetting = userState.userSettings?.firstWhere(
+      (setting) => setting.type == UserSettingType.GREETING,
+      orElse: () => UserSetting(value: 'Hi'),
+    );
 
-    final userName = ref.read(userViewModelProvider).user?.name ?? 'Guest';
+    final preferredGreeting = greetingSetting?.value ?? 'Hi';
+    String displayName = preferredName.isNotEmpty ? preferredName : userName;
 
     return ListTile(
       leading: const CircleAvatar(
         backgroundImage: AssetImage('assets/images/profile_temp.png'),
       ),
-      title: Text('$greeting, $userName'),
-      onTap: () {
-        // TODO: Navigate to profile edit screen
-      },
+      title: Text('$preferredGreeting, $displayName'),
     );
   }
 
@@ -383,12 +330,18 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     return ListTile(
       title: Text(title),
       trailing: const Icon(Icons.arrow_forward_ios),
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const EditProfileScreen(),
-          ),
-        );
+      onTap: () async {
+        bool isAuthenticated =
+            await showAuthenticationErrorCard(_scaffoldKey, ref);
+        if (isAuthenticated && mounted) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => EditProfileScreen(
+                updatePreferredName: _updatePreferredNameAPI,
+              ),
+            ),
+          );
+        }
       },
     );
   }
