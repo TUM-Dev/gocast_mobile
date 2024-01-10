ad1981b6de7742f4edacc2e85719367253fbca65
Saina Amiri Moghadam <amirimoghadamsaina@gmail.com>
2024-01-10 20:50:33 +0100

    move search bar from pinned_courses_content_view.dart to pinned_courses_view.dart, create searchbar

--- lib/views/course_view/pinned_courses_view/pinned_courses_view.dart
+++ lib/views/course_view/pinned_courses_view/pinned_courses_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
 import 'package:gocast_mobile/providers.dart';
+import 'package:gocast_mobile/views/components/custom_search_top_nav_bar.dart';
 import 'package:gocast_mobile/views/course_view/pinned_courses_view/pinned_card.dart';
 import 'package:gocast_mobile/views/course_view/course_detail_view/course_detail_view.dart';
 import 'package:gocast_mobile/views/course_view/pinned_courses_view/pinned_courses_content_view.dart';
@@ -15,6 +16,10 @@ class PinnedCourses extends ConsumerStatefulWidget {
 }
 
 class PinnedCoursesState extends ConsumerState<PinnedCourses> {
+  late List<Course> searchCourses;
+  bool isLoading = true;
+  final TextEditingController searchController = TextEditingController();
+
   Future<void> _refreshPinnedCourses() async {
     await ref.read(userViewModelProvider.notifier).fetchUserPinned();
   }
@@ -22,33 +27,68 @@ class PinnedCoursesState extends ConsumerState<PinnedCourses> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-      () => ref.read(userViewModelProvider.notifier).fetchUserPinned(),
-    );
+    _initializeCourses();
+    searchController.addListener(_searchCourses);
+  }
+
+  void _initializeCourses() {
+    final userViewModelNotifier = ref.read(userViewModelProvider.notifier);
+    Future.microtask(() async {
+      await userViewModelNotifier.fetchUserPinned();
+      if (mounted) {
+        setState(() {
+          searchCourses = ref.read(userViewModelProvider).userPinned ?? [];
+          isLoading = false; // Set isLoading to false here
+        });
+      }
+    });
+  }
+
+  void _searchCourses() {
+    final searchInput = searchController.text.toLowerCase();
+    final userPinned = ref.watch(userViewModelProvider).userPinned ?? [];
+
+    setState(() {
+      if (searchInput.isEmpty) {
+        searchCourses = userPinned;
+      } else {
+        searchCourses = userPinned.where((course) {
+          return course.name.toLowerCase().contains(searchInput) ||
+              course.slug.toLowerCase().contains(searchInput);
+        }).toList();
+        print('Filtered Courses Count: ${searchCourses.length}');
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final userPinned = ref.watch(userViewModelProvider).userPinned ?? [];
-    return RefreshIndicator(
-      onRefresh: _refreshPinnedCourses,
-      color: Colors.blue,
-      backgroundColor: Colors.white,
-      strokeWidth: 2.0,
-      displacement: 20.0,
-      child: PinnedCoursesContentView(
+    // final userPinned = ref.watch(userViewModelProvider).userPinned ?? [];
+    return Scaffold(
+      appBar: CustomSearchTopNavBar(
+        searchController: searchController,
         title: "Pinned Courses",
-        pinnedCourseCards: userPinned.map((course) {
-          final isPinned =
-              userPinned.any((pinnedCourse) => pinnedCourse.id == course.id);
-          return PinnedCourseCard(
-            imageName: 'assets/images/course2.png',
-            course: course,
-            onTap: () => _handleCourseTap(course, context),
-            isPinned: isPinned,
-            onPinToggle: () => _togglePin(course, isPinned),
-          );
-        }).toList(),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _refreshPinnedCourses,
+        color: Colors.blue,
+        backgroundColor: Colors.white,
+        strokeWidth: 2.0,
+        displacement: 20.0,
+        child: PinnedCoursesContentView(
+          title: "Pinned Courses",
+          pinnedCourseCards: searchCourses.map((course) {
+            final isPinned = searchCourses
+                .any((pinnedCourse) => pinnedCourse.id == course.id);
+            return PinnedCourseCard(
+              imageName: 'assets/images/course2.png',
+              course: course,
+              onTap: () => _handleCourseTap(course, context),
+              isPinned: isPinned,
+              onPinToggle: () => _togglePin(course, isPinned),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
