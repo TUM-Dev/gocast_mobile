057a6bcc66a69b531bfff941a3ae1e784a748be8
ge87joq <betul.cimendag@tum.de>
2024-01-10 17:45:40 +0100

    Add an error text to prevent a negative number from being entered in the Custom Playback Speed View

--- lib/views/settings_view/custom_playback_speed_view.dart
+++ lib/views/settings_view/custom_playback_speed_view.dart
@@ -5,46 +5,70 @@ void showAddCustomSpeedDialog(
   Function(double) onSpeedAdded,
 ) {
   double customSpeed = 1.0;
+  String errorMessage = '';
+
+  void validateAndSetSpeed(String value) {
+    if (value.isEmpty) {
+      errorMessage = '';
+    } else {
+      double? parsedValue = double.tryParse(value);
+      if (parsedValue != null && parsedValue > 0) {
+        customSpeed = parsedValue;
+        errorMessage = '';
+      } else {
+        errorMessage = 'Please enter a positive number';
+      }
+    }
+  }
 
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-        title: const Text('Add Custom Speed'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Enter speed (e.g., 1.7)',
-              ),
-              keyboardType:
-                  const TextInputType.numberWithOptions(decimal: true),
-              onChanged: (value) {
-                customSpeed = double.tryParse(value) ?? 1.0;
-              },
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
             ),
-          ],
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Cancel'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          TextButton(
-            child: const Text('Add'),
-            onPressed: () {
-              Navigator.of(context).pop();
-              onSpeedAdded(customSpeed);
-            },
-          ),
-        ],
+            backgroundColor: Theme.of(context).colorScheme.onPrimary,
+            title: const Text('Add Custom Speed'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter speed (e.g., 1.7)',
+                    errorText: errorMessage.isNotEmpty ? errorMessage : null,
+                  ),
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  onChanged: (value) {
+                    setState(() {
+                      validateAndSetSpeed(value);
+                    });
+                  },
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                onPressed: errorMessage.isEmpty && customSpeed > 0
+                    ? () {
+                        Navigator.of(context).pop();
+                        onSpeedAdded(customSpeed);
+                      }
+                    : null,
+                child: const Text('Add'),
+              ),
+            ],
+          );
+        },
       );
     },
   );
