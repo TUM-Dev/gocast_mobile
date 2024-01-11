c620a1d5dc6e25b5304fa98f6918a78d942de695
Achraf Labidi <achraf.labidi@tum.de>
2024-01-11 21:36:52 +0100

    Add pin course while watching video

--- lib/views/video_view/utils/custom_video_control_bar.dart
+++ lib/views/video_view/utils/custom_video_control_bar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
+import 'package:gocast_mobile/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 class CustomVideoControlBar extends StatelessWidget {
   final Function(String, Stream) onMenuSelection;
@@ -19,63 +22,74 @@ class CustomVideoControlBar extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
 
-    List<Map<String, IconData>> getMenuItems() {
+    List<Map<String, IconData>> getMenuItems(bool isPinned) {
       List<Map<String, IconData>> items = [
+        {'Pin Course': isPinned ? Icons.push_pin : Icons.push_pin_outlined},
         {'Download': Icons.file_download},
-        {'Share': Icons.share},
-        {'Pin Course': Icons.push_pin_outlined},
       ];
       return items;
     }
 
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Icons on the left
-          Row(
+    return Consumer(
+      builder: (context, ref, child) {
+        final userViewModel = ref.read(userViewModelProvider.notifier);
+        final isPinned = userViewModel.isCoursePinned(currentStream.courseID);
+
+        return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              IconButton(
-                icon: const Icon(Icons.messenger_outline_outlined),
-                color: themeData.iconTheme.color,
-                onPressed: onToggleChat,
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.messenger_outline_outlined),
+                    color: themeData.iconTheme.color,
+                    onPressed: onToggleChat,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.quiz_outlined),
+                    color: themeData.iconTheme.color,
+                    onPressed: onOpenQuizzes,
+                  ),
+                ],
               ),
-              IconButton(
-                icon: const Icon(Icons.quiz_outlined),
-                color: themeData.iconTheme.color,
-                onPressed: onOpenQuizzes,
+              PopupMenuButton<String>(
+                onSelected: (choice) async {
+                  if (choice == 'Pin Course') {
+                    isPinned
+                        ? await ref.read(userViewModelProvider.notifier).unpinCourse(currentStream.courseID)
+                        : await ref.read(userViewModelProvider.notifier).pinCourse(currentStream.courseID);
+                  } else {
+                    onMenuSelection(choice, currentStream);
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return getMenuItems(isPinned).map((Map<String, IconData> choice) {
+                    String text = choice.keys.first;
+                    IconData icon = choice.values.first;
+
+                    return PopupMenuItem<String>(
+                      value: text,
+                      child: ListTile(
+                        leading: Icon(icon, color: themeData.iconTheme.color),
+                        title: Text(text, style: TextStyle(color: themeData.textTheme.titleMedium?.color)),
+                      ),
+                    );
+                  }).toList();
+                },
+                icon: Icon(Icons.more_vert, color: themeData.iconTheme.color),
+                color: themeData.popupMenuTheme.color,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  side: BorderSide(color: themeData.dividerColor),
+                ),
+                elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0 : 3.0,
               ),
             ],
           ),
-
-          // Three-dot menu on the right
-          PopupMenuButton<String>(
-            onSelected: (choice) => onMenuSelection(choice, currentStream),
-            itemBuilder: (BuildContext context) {
-              return getMenuItems().map((Map<String, IconData> choice) {
-                String text = choice.keys.first;
-                IconData icon = choice.values.first;
-
-                return PopupMenuItem<String>(
-                  value: text,
-                  child: ListTile(
-                    leading: Icon(icon, color: themeData.iconTheme.color),
-                    title: Text(text, style: TextStyle(color: themeData.textTheme.titleMedium?.color)),
-                  ),
-                );
-              }).toList();
-            },
-            icon: Icon(Icons.more_vert, color: themeData.iconTheme.color),
-            color: themeData.popupMenuTheme.color,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              side: BorderSide(color: themeData.dividerColor),
-            ),
-            elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0 : 3.0,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
