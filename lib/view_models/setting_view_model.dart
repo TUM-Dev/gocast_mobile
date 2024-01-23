7445987476b5956a16eda4e827b16af6416e8e5a
betül çimendağ <betul.cimendag@tum.de>
2024-01-23 20:48:48 +0000

    205-fix-settings-view-in-lanscape-and-fix-updating-settings (#222)
    * Fix FocusScopeNode disposal issue in landscape mode on Settings View
    
    * Made the BaseView a Consumer
    
    * lint fix
    
    * Create `SettingState` and `SettingViewModel`
    
    * Rework the Settings updates
    
    * fix the `_showLogoutDialog` using Navigator in async gap
    
    * Add padding to setting view in Landscape mode
    
    * Sort `playBackSpeed` and disabled speed higher than `2.0`
    
    * Name shouldn´t be only empty spaces
    
    * add padding to edit profile view for landscape
    
    * show dialog box to handle error messages on edit profile view
    
    ---------
    
    Co-authored-by: Achraf Labidi <101757413+GravityDarkLab@users.noreply.github.com>
    Co-authored-by: Achraf Labidi <achraf.labidi@tum.de>

---
+++ lib/view_models/setting_view_model.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
+import 'package:gocast_mobile/base/networking/api/handler/grpc_handler.dart';
+import 'package:gocast_mobile/base/networking/api/handler/settings_handler.dart';
+import 'package:gocast_mobile/models/settings/setting_state_model.dart';
+import 'package:gocast_mobile/providers.dart';
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingViewModel extends StateNotifier<SettingState> {
+  final Logger _logger = Logger();
+  final GrpcHandler _grpcHandler;
+
+  SettingViewModel(this._grpcHandler) : super(const SettingState());
+
+  Future<void> fetchUserSettings() async {
+    try {
+      _logger.i('Fetching user settings..');
+      final userSettings =
+      await SettingsHandler(_grpcHandler).fetchUserSettings();
+      state = state.copyWith(userSettings: userSettings);
+      _logger.i('User settings fetched successfully');
+    } catch (e) {
+      _logger.e('Error fetching user settings: $e');
+    }
+  }
+
+  Future<void> updateUserSettings(List<UserSetting> updatedSettings) async {
+    _logger.i('Updating user settings..');
+    try {
+      final success = await SettingsHandler(_grpcHandler)
+          .updateUserSettings(updatedSettings);
+      if (success) {
+        state = state.copyWith(userSettings: updatedSettings);
+        _logger.i('User settings updated successfully');
+      } else {
+        _logger.e('Failed to update user settings');
+      }
+    } catch (e) {
+      _logger.e('Error updating user settings: $e');
+    }
+  }
+
+  Future<void> loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    await loadThemePreference(prefs);
+    await loadNotificationPreference(prefs);
+    await fetchUserSettings();
+  }
+
+  Future<void> loadThemePreference(SharedPreferences prefs) async {
+    final themePreference = prefs.getString('themeMode') ?? 'light';
+    state = state.copyWith(isDarkMode: themePreference == 'dark');
+  }
+
+  Future<void> saveThemePreference(String theme, WidgetRef ref) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('themeMode', theme);
+
+    state = state.copyWith(isDarkMode: theme == 'dark');
+
+    ref.read(themeModeProvider.notifier).state =
+    theme == 'dark' ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  Future<void> loadNotificationPreference(SharedPreferences prefs) async {
+    final notificationPreference = prefs.getBool('notifications') ?? true;
+    state = state.copyWith(isPushNotificationsEnabled: notificationPreference);
+  }
+
+  Future<void> saveNotificationPreference(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('notifications', value);
+    state = state.copyWith(isPushNotificationsEnabled: value);
+  }
+
+  Future<void> saveDownloadWifiOnlyPreference(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('downloadWifiOnly', value);
+    state = state.copyWith(isDownloadWithWifiOnly: value);
+  }
+
+  // This static method can stay within UserViewModel as it doesn't interact with the API
+  static List<double> getDefaultSpeeds() {
+    return List<double>.generate(14, (i) => (i + 1) * 0.25);
+  }
+
+  Future<void> updatePreferredGreeting(String newGreeting) async {
+    try {
+      await SettingsHandler(_grpcHandler)
+          .updateGreeting(newGreeting);
+      await fetchUserSettings();
+    } catch (e) {
+      _logger.e('Error updating greeting: $e');
+    }
+  }
+
+  Future<bool> updatePreferredName(String newName) async {
+    try {
+      await SettingsHandler(_grpcHandler)
+          .updatePreferredName(newName);
+      await fetchUserSettings();
+      return true;
+    } catch (e) {
+      _logger.e('Error updating preferred name: $e');
+      return false;
+    }
+  }
+
+  Future<void> updateSelectedSpeeds(double speed, bool isSelected) async {
+    await SettingsHandler(_grpcHandler)
+        .updateSelectedSpeeds(speed, isSelected, state.userSettings ?? []);
+    await fetchUserSettings();
+  }
+
+  List<double> parsePlaybackSpeeds() {
+    return SettingsHandler(_grpcHandler)
+        .parsePlaybackSpeeds(state.userSettings);
+  }
+
+
+  List<UserSetting>? getUserSettings() {
+    return state.userSettings;
+  }
+
+  void setLoading(bool isLoading) {
+    state = state.copyWith(isLoading: isLoading);
+  }
+
+}
--- lib/view_models/user_view_model.dart
+++ lib/view_models/user_view_model.dart
@@ -2,22 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gocast_mobile/base/networking/api/gocast/api_v2.pb.dart';
 import 'package:gocast_mobile/base/networking/api/handler/auth_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/bookmarks_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/course_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/grpc_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/pinned_handler.dart';
-import 'package:gocast_mobile/base/networking/api/handler/settings_handler.dart';
-
 import 'package:gocast_mobile/base/networking/api/handler/token_handler.dart';
 import 'package:gocast_mobile/base/networking/api/handler/user_handler.dart';
 import 'package:gocast_mobile/models/error/error_model.dart';
 import 'package:gocast_mobile/models/user/user_state_model.dart';
-import 'package:gocast_mobile/providers.dart';
 import 'package:gocast_mobile/utils/globals.dart';
 import 'package:logger/logger.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class UserViewModel extends StateNotifier<UserState> {
   final Logger _logger = Logger();
@@ -35,7 +30,6 @@ class UserViewModel extends StateNotifier<UserState> {
       _logger.i('Logging in user with email: $email');
       await AuthHandler.basicAuth(email, password);
       await fetchUser();
-      await fetchUserSettings();
       _logger.i('Logged in user with basic auth');
 
       if (state.user != null) {
@@ -138,34 +132,6 @@ class UserViewModel extends StateNotifier<UserState> {
     }
   }
 
-  Future<void> fetchUserSettings() async {
-    try {
-      _logger.i('Fetching user settings..');
-      final userSettings =
-          await SettingsHandler(_grpcHandler).fetchUserSettings();
-      state = state.copyWith(userSettings: userSettings);
-      _logger.i('User settings fetched successfully');
-    } catch (e) {
-      _logger.e('Error fetching user settings: $e');
-    }
-  }
-
-  Future<void> updateUserSettings(List<UserSetting> updatedSettings) async {
-    _logger.i('Updating user settings..');
-    try {
-      final success = await SettingsHandler(_grpcHandler)
-          .updateUserSettings(updatedSettings);
-      if (success) {
-        state = state.copyWith(userSettings: updatedSettings);
-        _logger.i('User settings updated successfully');
-      } else {
-        _logger.e('Failed to update user settings');
-      }
-    } catch (e) {
-      _logger.e('Error updating user settings: $e');
-    }
-  }
-
   Future<void> fetchUserBookmarks() async {
     state = state.copyWith(isLoading: true);
     try {
@@ -196,88 +162,8 @@ class UserViewModel extends StateNotifier<UserState> {
     _logger.i('Logged out user and cleared tokens.');
   }
 
-  void setLoading(bool isLoading) {
-    state = state.copyWith(isLoading: isLoading);
-  }
 
-  Future<void> loadPreferences() async {
-    final prefs = await SharedPreferences.getInstance();
-    await loadThemePreference(prefs);
-    await loadNotificationPreference(prefs);
-    await fetchUserSettings();
-  }
-
-  Future<void> loadThemePreference(SharedPreferences prefs) async {
-    final themePreference = prefs.getString('themeMode') ?? 'light';
-    state = state.copyWith(isDarkMode: themePreference == 'dark');
-  }
-
-  Future<void> saveThemePreference(String theme, WidgetRef ref) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('themeMode', theme);
-
-    state = state.copyWith(isDarkMode: theme == 'dark');
-
-    ref.read(themeModeProvider.notifier).state =
-        theme == 'dark' ? ThemeMode.dark : ThemeMode.light;
-  }
-
-  Future<void> loadNotificationPreference(SharedPreferences prefs) async {
-    final notificationPreference = prefs.getBool('notifications') ?? true;
-    state = state.copyWith(isPushNotificationsEnabled: notificationPreference);
-  }
-
-  Future<void> saveNotificationPreference(bool value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('notifications', value);
-    state = state.copyWith(isPushNotificationsEnabled: value);
-  }
-
-  Future<void> saveDownloadWifiOnlyPreference(bool value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('downloadWifiOnly', value);
-    state = state.copyWith(isDownloadWithWifiOnly: value);
-  }
-
-  // This static method can stay within UserViewModel as it doesn't interact with the API
-  static List<double> getDefaultSpeeds() {
-    return List<double>.generate(14, (i) => (i + 1) * 0.25);
-  }
-
-  Future<void> updatePreferredGreeting(String newGreeting) async {
-    try {
-      await SettingsHandler(_grpcHandler)
-          .updatePreferredGreeting(newGreeting, state.userSettings ?? []);
-      await fetchUserSettings();
-    } catch (e) {
-      _logger.e('Error updating greeting: $e');
-    }
-  }
-
-  Future<bool> updatePreferredName(String newName) async {
-    try {
-      await SettingsHandler(_grpcHandler)
-          .updatePreferredName(newName, state.userSettings ?? []);
-      await fetchUserSettings();
-      return true;
-    } catch (e) {
-      _logger.e('Error updating preferred name: $e');
-      return false;
-    }
-  }
-
-  Future<void> updateSelectedSpeeds(double speed, bool isSelected) async {
-    await SettingsHandler(_grpcHandler)
-        .updateSelectedSpeeds(speed, isSelected, state.userSettings ?? []);
-    await fetchUserSettings();
-  }
-
-  List<double> parsePlaybackSpeeds() {
-    return SettingsHandler(_grpcHandler)
-        .parsePlaybackSpeeds(state.userSettings);
-  }
-
-  isCoursePinned(int id) {
+ bool  isCoursePinned(int id) {
     if (state.userPinned == null) {
       return false;
     }
@@ -289,7 +175,8 @@ class UserViewModel extends StateNotifier<UserState> {
     return false;
   }
 
-  List<UserSetting>? getUserSettings() {
-    return state.userSettings;
+  void setLoading(bool loading) {
+    state = state.copyWith(isLoading: loading);
   }
+
 }
