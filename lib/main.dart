be7f5a4bce65ed734d5ae5bb396aabe215624b54
Achraf Labidi <achraf.labidi@tum.de>
2024-02-02 13:27:16 +0100

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:gocast_mobile/utils/UserPreferences.dart';
 import 'package:gocast_mobile/utils/globals.dart';
 import 'package:gocast_mobile/utils/theme.dart';
 import 'package:gocast_mobile/navigation_tab.dart';
+import 'package:gocast_mobile/views/course_view/downloaded_courses_view/downloaded_courses_view.dart';
 import 'package:gocast_mobile/views/course_view/list_courses_view/public_courses_view.dart';
 import 'package:gocast_mobile/views/login_view/internal_login_view.dart';
 import 'package:gocast_mobile/views/on_boarding_view/welcome_screen_view.dart';
@@ -51,6 +52,7 @@ class App extends ConsumerWidget {
     _setupNotifications(ref, userState);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -91,6 +93,7 @@ class App extends ConsumerWidget {
       '/login': (context) => const InternalLoginScreen(),
       '/navigationTab': (context) => const NavigationTab(),
       '/publiccourses': (context) => const PublicCourses(),
+      '/downloads': (context) => const DownloadedCourses(),
     };
   }
 
